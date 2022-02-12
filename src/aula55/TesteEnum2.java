686152042865e889410ce720251d17b0420bcebe
jessicacorreaes <jessicacorreaes@gmail.com>
2022-02-11 21:18:02 -0300

    Aula 55

---
+++ src/aula55/TesteEnum2.java
@@ -0,0 +1,16 @@
+package aula55;
+
+import aula53.DiaSemana;
+
+public class TesteEnum2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Enum.valueOf(DiaSemana.class, "DOMINGO"));
+
+        DiaSemana dia = Enum.valueOf(DiaSemana.class, "DOMINGO");
+
+        System.out.println(dia);
+    }
+
+}
